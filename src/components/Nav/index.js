521a1fcea08bb9514a9cc19dae93e9c06a6bf514
sarvanideekshitula <sarvanimini@gmail.com>
2020-02-18 10:34:06 +0530

    feat(Nav): Add Nav component and css

---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function Nav() {
+  return (
+    <div className="NavBar">
+      <p>
+        <b>Record</b>
+        Shelf
+      </p>
+    </div>
+  );
+}
+
+
+export default Nav;
